6800332e14eb55414eb7b536206add3b26740b18
Cristian Stoica <cristianstoica85@gmail.com>
2023-09-02 01:47:56 +0300

    Updated script to install the latest development build for WiX Toolset v3.

--- Scripts/Install-Wix.ps1
+++ Scripts/Install-Wix.ps1
@@ -11,4 +11,4 @@ Import-Module -Name "$PSScriptRoot\ImageHelpers" -Verbose
 $temp_install_dir = 'C:\Windows\Installer'
 New-Item -Path $temp_install_dir -ItemType Directory -Force
 
-Install-EXE -Url "https://wixtoolset.org/downloads/v3.14.0.4118/wix314.exe" -Name "wix314.exe" -ArgumentList "/quiet"
+Install-EXE -Url "https://wixtoolset.org/downloads/v3.14.0.6526/wix314.exe" -Name "wix314.exe" -ArgumentList "/quiet"
